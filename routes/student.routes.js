218191bcb0cfd0194644117a8202c2daa8c4cbaf
Ankur Santra <santraankur47@gmail.com>
2026-01-19 10:45:09 +0530

    /

--- routes/student.routes.js
+++ routes/student.routes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+
 router.get("/", (req, res) => { res.end('Student working successfully'); });
 
 router.get("/add", (req, res) => { res.end('Add student working successfully'); });
